beb7e939d59efe1f6d3b88da0da801c676817fba
Tim Stack <tstack43215@yahoo.com>
2023-02-10 15:42:59 -0600

    Initial Commit

---
+++ Overlap Integral/Overlap_IntegralApp.swift
@@ -0,0 +1,17 @@
+//
+//  Overlap_IntegralApp.swift
+//  Overlap Integral
+//
+//  Created by IIT PHYS 440 on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Overlap_IntegralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Overlap IntegralUITests/Overlap_IntegralUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Overlap_IntegralUITestsLaunchTests.swift
+//  Overlap IntegralUITests
+//
+//  Created by IIT PHYS 440 on 2/10/23.
+//
+
+import XCTest
+
+final class Overlap_IntegralUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
